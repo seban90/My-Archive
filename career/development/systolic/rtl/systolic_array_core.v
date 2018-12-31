2540859f8486e62a337fcc4f80e8f31d503a588c
seban90 <seban90@naver.com>
2018-12-31 13:04:40 +0900

    systolic arrary core

--- career/development/systolic/rtl/systolic_array_core.v
+++ career/development/systolic/rtl/systolic_array_core.v
@@ -12,19 +12,23 @@ module systolic_array_core
 	,input [COLS-1:0]              i_B_VALID
 
 	,output [ROW*COLS*2*DBITS-1:0] o_DATA
-	,output [ROW*COLS-1:0]         o_DATA_VALID
+	,output [ROW*COLS-1:0]         o_VALID
 
 	,output [2*DBITS-1:0]          acc
 	,output                        acc_valid
 );
 
-	genvar g;
-	generate
-
 	wire [ROWS*DBITS-1:0] nst_row_data[0:COLS-1];
 	wire [ROWS-1      :0] nst_row_valid[0:COLS-1];
 	wire [COLS*DBITS-1:0] nst_col_data[0:ROWS];
 	wire [COLS-1      :0] nst_col_valid[0:ROWS];
+
+	wire [ROWS*2*DBITS-1:0] out_data[0:COLS-1];
+	wire [ROWS-1      :0] out_valid[0:COLS-1];
+
+	genvar g;
+	generate
+
 
 	assign nst_col_data[0]  = i_B;
 	assign nst_col_valid[0] = i_B_VALID;
@@ -38,8 +42,8 @@ module systolic_array_core
 			 .DBITS (DBITS)
 			,.ACC   (ROWS)
 		) (
-			 .CLK           (  {(ROWS){i_CLK}}       )
-			,.RSTN          (  {(ROWS){i_RSTN}}      )
+			 .CLK           (  i_CLK                 )
+			,.RSTN          (  i_RSTN                )
 			,.DATA_A        (  row_data_gen          )
 			,.DATA_B        (  nst_col_data[g]       )
 			,.VALID_A       (  row_valid_gen         )
@@ -48,12 +52,15 @@ module systolic_array_core
 			,.NEXT_DATA_B   (  nst_col_data[g+1]     )
 			,.NEXT_VALID_A  (  nst_row_valid[g]      )
 			,.NEXT_VALID_B  (  nst_col_valid[g+1]    )
-			,.OUT_DATA      (  OUT_DATA              )
-			,.OUT_VALID     (  OUT_VALID             )
+			,.OUT_DATA      (  out_data[g]           )
+			,.OUT_VALID     (  out_valid[g]          )
 		);
 
 		assign row_data_gen  = {nst_row_data[g], i_A[(g+1)*DBITS-1:g*DBITS]};
 		assign row_valid_gen = {nst_row_valid[g], i_A_VALID[g:g]};
+
+		assign o_DATA[(g+1)*(ROWS*2*DBITS)-1:g*(ROWS*2*DBITS)] = out_data[g];
+		assign o_VALID[(g+1)*ROWS-1:g*ROWS]                    = out_valid[g];
 	end
 	endgenerate
 
